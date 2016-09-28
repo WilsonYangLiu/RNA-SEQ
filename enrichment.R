eb55f5dbf3577dc360836d6b88a28e5b3d722da9
WilsonYangLiu <lweixin316@gmail.com>
2016-09-28 10:10:53 +0800

    Add folder [diff_Sb-2w_result] to .gitignore

--- enrichment.R
+++ enrichment.R
@@ -1,13 +1,12 @@
 rm(list = ls())
 setwd(dir = '/media/wilson/b776f228-366c-4e52-acd6-65df5b458e8c/Project_s542r04002/')
 
-library(gplots)
+library(ggplot2)
 
 off.2w <- read.table(file = 'diff_off-2w_result/DAVID-kegg.enrichment', header = TRUE, sep = '\t')
 Sb.2w <- read.table(file = 'diff_Sb-2w_result/DAVID-kegg.enrichment', header = TRUE, sep = '\t')
 
 off.name <- as.data.frame(strsplit(as.character(off.2w$Term), ':'))
-bp <- barplot2(height = off.2w$Count, names.arg = NULL, horiz = TRUE, 
-         axes = FALSE)
-axis(side = 2, at = bp, labels = as.character(t(off.name[2,])))
-axis(side = 3, at = bp, labels = as.character(t(off.name[2,])))
+ggplot(off.2w,aes(x=factor(Count)))
+
+#as.character(t(off.name[2,]))
