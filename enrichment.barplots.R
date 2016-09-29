ff42385b2e52e834cea0533fd73955a102f1bdd8
WilsonYangLiu <lweixin316@gmail.com>
2016-09-29 21:26:51 +0800

    Add enrichment.barplots.R

--- enrichment.barplots.R
+++ enrichment.barplots.R
@@ -1,12 +1,15 @@
 rm(list = ls())
 setwd(dir = '/media/wilson/b776f228-366c-4e52-acd6-65df5b458e8c/Project_s542r04002/')
 
-library(ggplot2)
+library(gplots)
 
 off.2w <- read.table(file = 'diff_off-2w_result/DAVID-kegg.enrichment', header = TRUE, sep = '\t')
 Sb.2w <- read.table(file = 'diff_Sb-2w_result/DAVID-kegg.enrichment', header = TRUE, sep = '\t')
 
 off.name <- as.data.frame(strsplit(as.character(off.2w$Term), ':'))
-ggplot(off.2w,aes(x=factor(Count)))
+off <- off.2w$Count
+names(off) <- as.character(t(off.name[2,]))
+off <- sort(off)
 
-#as.character(t(off.name[2,]))
+bp <- barplot2(height = off, horiz = TRUE, axes = FALSE, las = 1)
+axis(side = 3)
